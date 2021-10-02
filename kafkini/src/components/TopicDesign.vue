6ddbec55d79c5248c70ab4271dad3ae5ec6577a9
amfl <amfl@none.none>
2021-10-02 21:19:13 +1300

    All the dropdowns

--- kafkini/src/components/TopicDesign.vue
+++ kafkini/src/components/TopicDesign.vue
@@ -4,10 +4,27 @@
   </span></h3>
 
   <form>
-    <label for="msgType">Type: </label><input type="text" id="msgType" v-model="msgType"><br>
-    <label for="msgType">Domain: </label><input type="text" id="msgDomain" v-model="msgDomain"><br>
+    <label for="msgType">Type: </label>
+    <select id="msgType" v-model="msgType">
+      <option disabled value="">Select type</option>
+      <option v-for="type in types()" v-bind:key="type">{{type}}</option>
+    </select>
+    <br>
+
+    <label for="msgType">Domain: </label>
+    <select id="msgEnv" v-model="msgDomain">
+      <option disabled value="">Select domain</option>
+      <option v-for="domain in domains()" v-bind:key="domain">{{domain}}</option>
+    </select>
+    <br>
+
     <label for="msgType">Dataset: </label><input type="text" id="msgDesc" v-model="msgDesc"><br>
-    <label for="msgType">Environment: </label><input type="text" id="msgEnv" v-model="msgEnv"><br>
+
+    <label for="msgType">Environment: </label>
+    <select id="msgEnv" v-model="msgEnv">
+      <option disabled value="">Select environment</option>
+      <option v-for="env in envs()" v-bind:key="env">{{env}}</option>
+    </select>
   </form>
 
   <br>
@@ -52,6 +69,8 @@ resource "kafka_topic" "logs" {
         msgDesc: "donut service",
         msgEnv: "prod",
 
+        selectedEnv: null,
+
         replicationFactor: 3,
         partitions: 3,
 
@@ -60,11 +79,27 @@ resource "kafka_topic" "logs" {
     },
     methods: {
         getTopicName() {
+            // TODO: handle periods in here
             let topicName = this.msgType + '.' + this.msgDomain + '.' + this.msgDesc + '.' + this.msgEnv
             topicName = topicName.toLowerCase()
-            topicName = topicName.replace(' ', '_');
-            topicName = topicName.replace('-', '_');
+            topicName = topicName.replaceAll(' ', '_');
+            topicName = topicName.replaceAll('-', '_');
             return topicName;
+        },
+        envs() {
+            return [
+                'dev','test', 'uat', 'prod'
+            ];
+        },
+        types() {
+            return [
+                'streaming','fact', 'cdc'
+            ];
+        },
+        domains() {
+            return [
+                'cards','finance', 'customer', 'something else'
+            ];
         }
     }
   }
